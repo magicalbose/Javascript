1010964db52a9d938aa8a33385b5931a209a33e4
magicalbose <peehana.bose@gmail.com>
2025-05-27 21:07:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function SumOfTwoNumbers(){
+    var x, y, z;
+    x = 5;
+    y = 6;
+    z = x + y;
+
+    return z;
+}
+var sum = SumOfTwoNumbers();
+document.write('The sum of x and y is' + sum)
