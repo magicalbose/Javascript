1fb0d1d122dbaf9f72980a7d5e1a12567996f767
magicalbose <peehana.bose@gmail.com>
2025-05-27 21:17:18 -0500

    Add files via upload

---
+++ oddEven.js
@@ -0,0 +1,10 @@
+var OddOrEven = function(number){
+    if(number % 2 == 0) {
+        document.write(number + ' is an even number.');
+    }
+    else {
+        document.write(number + ' is an odd number.');
+    }
+}
+
+OddOrEven(81)
