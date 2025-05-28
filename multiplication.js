5c8c7bd4eea5c6c34ff9f7b7c5ca4500bf4913b2
magicalbose <peehana.bose@gmail.com>
2025-05-27 21:37:19 -0500

    Add files via upload

---
+++ multiplication.js
@@ -0,0 +1,10 @@
+function ProductOfTwoNumbers() {
+    var x, y, z;
+    x = 3;
+    y = 4;
+    z = x * y;
+
+    return z;
+}
+var multiplication = ProductOfTwoNumbers();
+document.write('The product of x and y is ' +multiplication);
