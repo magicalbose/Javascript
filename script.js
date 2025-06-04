313c309421e67888de9efc503d9f753757980ffd
magicalbose <peehana.bose@gmail.com>
2025-06-03 21:20:41 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let div = document.createElement('div')
+
+
+div.id = "paragraph";
+div.className = "alert";
+div.innerHTML = "<p>Congrats! You have created a div.</p>";
+
+
+document.body.append(div);
+
+
+let heading = document.getElementById("heading");
+heading.innerHTML = '&#128512';
+
+
+document.querySelector('header p').innerHTML = "<strong>Good Job!</strong>";
