a88a4f2780cf81df5e573223de8c6ef63f9c7e86
magicalbose <peehana.bose@gmail.com>
2025-06-09 21:15:44 -0500

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,12 @@
+let solarSystem = document.createElement('h1');
+
+
+solarSystem.id = "sS";
+solarSystem.className = "sun";
+solarSystem.innerHTML = "The Solar System";
+
+
+document.body.prepend(solarSystem);
+
+
+let heading = document.getElementById('planets');
