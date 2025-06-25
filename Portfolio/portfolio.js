1a95a21deaf8df743ca4e76d1da8a46289d67e11
magicalbose <peehana.bose@gmail.com>
2025-06-24 21:22:41 -0500

    Add files via upload

---
+++ Portfolio/portfolio.js
@@ -0,0 +1,9 @@
+function tiger() {
+    document.getElementById("tiger-info").innerText = "Tigers are the largest cat species in the world and are known for their power and strength.";
+}
+
+//Both infos for the animals
+
+function lion() {
+    document.getElementById("lion-info").innerText = "Lions are social cats and live in groups called prides. They are often referred to as the 'king of the jungle'.";
+}
