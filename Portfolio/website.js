143b563b92fb77f8d49305ba5546024796fe8cd9
magicalbose <peehana.bose@gmail.com>
2025-07-08 13:33:06 -0500

    Add files via upload

---
+++ Portfolio/website.js
@@ -0,0 +1,6 @@
+function chess() {
+    document.getElementById("chess-info").innerText = "Chess is a two-player strategy board game played on a checkered board with 64 squares. The game involves two players, each controlling 16 pieces: one king, one queen, two rooks, two bishops, two knights, and eight pawns. The objective is to checkmate the opponent's king, meaning to threaten it with capture and leave it with no legal moves.";
+}
+function badminton() {
+    document.getElementById("badminton-info").innerText = "Badminton is a racket sport played with a shuttlecock (also called a birdie) and rackets, either by two players (singles) or two teams of two (doubles). The goal is to hit the shuttlecock over a net and make it land within the opponent's half of the court, while preventing them from doing the same. Points are scored when the shuttlecock lands within the opponent's designated area or when they commit a fault." ;
+}
