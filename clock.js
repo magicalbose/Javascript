df13b679273594b37fd6e29d742f05fdff2f50db
magicalbose <peehana.bose@gmail.com>
2026-05-15 21:02:52 -0500

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,22 @@
+function showTime(){
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    var session = 'AM'
+
+    if (hours > 12){
+        hours = hours - 12
+        session = 'PM'
+    }
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    var time = hours + ":" + minutes + ":" + seconds + " " + session
+    document.getElementById('MyClockDisplay').innerHTML = time
+    setTimeout(showTime, 1000)
+}
+
+showTime()
